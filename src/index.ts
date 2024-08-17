53a40c856a8251938a3c38f13dd23820fddd0514
zxkmm <zxkmm@hotmail.com>
2024-08-17 17:28:27 +0800

    fix css and don't display if 100%

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-import {
-  Plugin,
-  showMessage,
-  IModel,
-} from "siyuan";
+import { Plugin, showMessage, IModel } from "siyuan";
 import "@/index.scss";
-
 
 import { SettingUtils } from "./libs/setting-utils";
 
@@ -36,19 +31,27 @@ export default class siyuan_global_zoom extends Plugin {
       type: "textinput",
       title: this.i18n.baseZoomNumber,
       description: this.i18n.baseZoomNumberDesc,
-  });
+    });
 
-  this.settingUtils.addItem({
-    key: "mainShowZoomInfo",
-    value: 1,
-    type: "select",
-    title: this.i18n.mainShowZoomInfo,
-    description: this.i18n.mainShowZoomInfoDesc,
-    options: {
-      1: "100%",
-      2: "1×",
+    this.settingUtils.addItem({
+      key: "mainShowZoomInfo",
+      value: 1,
+      type: "select",
+      title: this.i18n.mainShowZoomInfo,
+      description: this.i18n.mainShowZoomInfoDesc,
+      options: {
+        1: "100%",
+        2: "1×",
       },
-  });
+    });
+
+    this.settingUtils.addItem({
+      key: "dontDisplayIf100",
+      value: false,
+      type: "checkbox",
+      title: this.i18n.dontDisplayIf100,
+      description: "",
+    });
 
     this.settingUtils.addItem({
       key: "Hint",
@@ -69,91 +72,79 @@ export default class siyuan_global_zoom extends Plugin {
     console.log(this.i18n.helloPlugin);
   }
 
-  // rescuveDangerousZoom() {
-  //   const dprDisplay = document.createElement("div");
-  //   const zoomLevelHint = document.createElement("div");
-
-  //   dprDisplay.style.cssText = `
-  //       position: fixed;
-  //       top: 50%;
-  //       left: 50%;
-  //       transform: translate(-50%, -50%);
-  //       font-size: 24px;
-  //       background-color: rgba(0, 0, 0, 0.99);
-  //       color: red;
-  //       padding: 10px;
-  //       border-radius: 5px;
-  //       z-index: 9999;
-  //       `;
-
-  //   document.body.appendChild(dprDisplay);
-  //   document.body.appendChild(zoomLevelHint);
-
-  //   const hintBefore = `使用方法：（1）新建一个工作空间，安装并启用此插件，查看并记住条目7所示的正常默认的缩放比例（一般是1.25)
-  //   ； （2）在您不小心使用ctrl（或control）加滚轮或ctrl（或control）和+-键盘缩放的主工作空间安装并启用此插件
-  //   ； （3）使用您之前不小心调乱缩比例的方法（要么是ctrl（control）+滚轮，要么是（ctrlcontrol）和+-）
-  //   ； （4）来调整回你在新工作空间看到的比例（一般都是1.25）
-  //   ；（5）此时您的工作空间的缩放比例已经还原到默认值。
-  //   ；（6）最后卸载或禁用本插件即可。
-  //   ；（7）请注意，当前的比例为：------>[[[[[[[[[[[`;
-
-  //   const hintAfter = `]]]]]]]]]]]<------`;
-
-  //   function updateDprDisplay() {
-  //     dprDisplay.textContent =
-  //       hintBefore + `${window.devicePixelRatio}` + hintAfter;
-  //   }
-
-  //   updateDprDisplay();
-
-  //   window.addEventListener("resize", updateDprDisplay);
-  // }
-
   showZoomInfoAndCallback() {
     const _mainShowZoomInfo_ = this.settingUtils.get("mainShowZoomInfo");
     const _baseZoomNumber_ = this.settingUtils.get("baseZoomNumber");
+    const _dontDisplayIf100_ = this.settingUtils.get("dontDisplayIf100");
     const zoomInfoElement = document.createElement("div");
-    zoomInfoElement.classList.add("toolbar__item", "ariaLabel");
-    if(_mainShowZoomInfo_ == 1){
+    zoomInfoElement.classList.add("status__counter"); //套用字符数的css照顾第三方主主题
+    if (_mainShowZoomInfo_ == 1) {
       zoomInfoElement.setAttribute("aria-label", "AKA 1×");
-    } else if(_mainShowZoomInfo_ == 2){
+    } else if (_mainShowZoomInfo_ == 2) {
       zoomInfoElement.setAttribute("aria-label", "AKA 100%");
     }
-  
+
+    zoomInfoElement.addEventListener("click", () => {
+      //this call back works, just TODO
+    });
+
     const updateZoomInfo = () => {
       if (_mainShowZoomInfo_ == 1) {
-      zoomInfoElement.textContent = `${(window.devicePixelRatio / _baseZoomNumber_ * 100).toFixed(0)}%`;
-      zoomInfoElement.setAttribute("aria-label", `AKA ${window.devicePixelRatio}×`);
-      } else if (_mainShowZoomInfo_ == 2){
-      zoomInfoElement.textContent = `${window.devicePixelRatio}×`;
-      zoomInfoElement.setAttribute("aria-label", `AKA ${(window.devicePixelRatio / _baseZoomNumber_ * 100).toFixed(0)}%`);
+        zoomInfoElement.textContent = `${(
+          (window.devicePixelRatio / _baseZoomNumber_) *
+          100
+        ).toFixed(0)}%`;
+        zoomInfoElement.setAttribute(
+          "aria-label",
+          `AKA ${window.devicePixelRatio}×`
+        );
+
+        if (_dontDisplayIf100_) {
+          if (window.devicePixelRatio == _baseZoomNumber_) {
+            zoomInfoElement.style.display = "none";
+          } else {
+            zoomInfoElement.style.display = "block";
+          }
+        }
+      } else if (_mainShowZoomInfo_ == 2) {
+        zoomInfoElement.textContent = `${window.devicePixelRatio}×`;
+        zoomInfoElement.setAttribute(
+          "aria-label",
+          `AKA ${((window.devicePixelRatio / _baseZoomNumber_) * 100).toFixed(
+            0
+          )}%`
+        );
+
+        if (_dontDisplayIf100_) {
+          if (window.devicePixelRatio == _baseZoomNumber_) {
+            zoomInfoElement.style.display = "none";
+          } else {
+            zoomInfoElement.style.display = "block";
+          }
+        }
       }
     };
-  
+
     updateZoomInfo();
-  
-    window.addEventListener('resize', updateZoomInfo);
-  
+
+    window.addEventListener("resize", updateZoomInfo);
+
     this.addStatusBar({
       element: zoomInfoElement,
     });
   }
 
   onLayoutReady() {
-    if(this.settingUtils.get("mainSwitch")){
-    // this.rescuveDangerousZoom();
-    this.showZoomInfoAndCallback();
-
+    if (this.settingUtils.get("mainSwitch")) {
+      // this.rescuveDangerousZoom();
+      this.showZoomInfoAndCallback();
     }
 
     // this.loadData(STORAGE_NAME);
     this.settingUtils.load();
   }
 
-  async onunload() {
+  async onunload() {}
 
-  }
-
-  uninstall() {
-  }
+  uninstall() {}
 }
